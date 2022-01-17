754f4d79aef3ef3ec71ea44c274b6860fbf36442
walsonFerrao <96103529+walsonFerrao@users.noreply.github.com>
2022-01-17 16:06:59 +0530

    commiting
    commiting

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,6 +1,18 @@
 function sidebar() {
 
+return ` <div><h1>TechCrunch</h1></div>
+<div><button>login</button></div>
+<br>
+<div><input type="text" placeholder="Search News"> <button>search</button></div>
+<br>
+<div>startups</div>
+<div>newsletters</div>
+<div>audio</div>
+<div>video</div>
 
+
+
+</div>`
     // return your html component here
     //Make sure to give input search box id as ""
 }
--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,55 @@ async function apiCall(url) {
 
     //add api call logic here
 
+   let  req=await fetch(url)
+  let   res=await req.json()
 
+  return res
 }
 
 
 function appendArticles(articles, main) {
 
+    articles.map((elem)=>{
+
+
+        let div1=document.createElement("div")
+        let div2=document.createElement("div")
+        let div3=document.createElement("div")
+        
+         let title=document.createElement("p")
+         title.innerHTML=elem.title
+     
+         let description=document.createElement("p")
+         description.innerHTML=elem.description
+     
+        let img=document.createElement("img")
+        img.src=elem.image
+     
+        let time=document.createElement("img")
+        time.innerHTML=elem.publishedAt
+     
+     //    appending
+     
+     div2.append(img)
+     div3.append(time,title,description)
+     div1.append(div2,div3)
+     main.append(div1)
+
+
+     div1.addEventListener("click",collectdata)
+     function collectdata(){
+         localStorage.setItem("article",JSON.stringify(elem))
+         window.location.href="news.html"
+     }
+
+
+    })
     //add append logic here
+  
+
+
+   
 
 }
 
